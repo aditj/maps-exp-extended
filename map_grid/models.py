299e4b770609b5041a2ceead5eab27f4420ef46c
Adit Jain <aditjain1980@gmail.com>
2022-02-26 17:48:20 +0530

    added first

---
+++ map_grid/models.py
@@ -0,0 +1,110 @@
+from otree.api import (
+    models,
+    widgets,
+    BaseConstants,
+    BaseSubsession,
+    BaseGroup,
+    BasePlayer,
+    Currency as c,
+    currency_range,
+)
+
+
+author = 'Adit Jain'
+
+doc = """
+Your app description
+"""
+import numpy as np
+
+class Constants(BaseConstants):
+    name_in_url = 'map_grid'
+    players_per_group = 2
+    num_rounds = 1
+    
+    grid_size = (5, 5)
+    grid_dim = grid_size[0]
+    grid_x = np.arange(grid_size[0],dtype=int)
+    grid_y = np.arange(grid_size[1],dtype=int)
+    grid_value = [
+        [1,2,4,1,3,5,2,1,3,4],
+        [2,3,1,4,2,1,3,4,5,2],
+        [4,1,3,2,5,3,1,2,4,5],
+        [1,4,2,3,1,5,4,3,2,5],
+        [3,2,5,1,4,2,5,1,3,4],
+        [5,3,4,2,1,4,3,2,5,1],
+        [2,1,3,5,3,2,4,1,5,3],
+        [3,4,1,2,5,1,2,3,4,5],
+        [1,2,5,4,2,3,1,5,3,4],
+        [4,5,2,1,3,5,4,2,1,3]
+    ]
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+
+class Player(BasePlayer):
+    name = models.StringField()
+    chance = models.IntegerField(min=0, max=1)
+    points = models.IntegerField()
+    current_x = models.IntegerField()
+    current_y = models.IntegerField()
+    current_payoff = models.IntegerField(  default=0)
+    current_chance = models.IntegerField(  default=0)
+
+    
+
+class Element(models.Model):
+    value = models.IntegerField()
+    x = models.IntegerField()
+    y = models.IntegerField()
+    player = models.Link(Player)
+    class CustomModelConf:
+            """
+            Configuration for otreeutils admin extensions.
+            """
+
+            data_view = {  # define this attribute if you want to include this model in the live data view
+                "link_with": "player"
+            }
+            export_data = {  # define this attribute if you want to include this model in the data export
+                "link_with": "player"
+            }
+
+def update_element(self,data):
+    x = int(data['current_x'])
+    y = int(data['current_y'])
+    value = int(Constants.grid_value[x][y])
+    self.current_payoff += value
+    for player in self.group.get_players():
+        player.current_chance += 1
+        if player!=self:
+            opponent_id = player.id_in_group
+            opponent_payoff = player.current_payoff
+
+
+    e = Element(value = value, x=  x,y =  y,player = self)
+    e.save()
+    return {self.id_in_group:{
+       "data":data,
+        "was_opponent":0,
+        "player_payoff":self.current_payoff,
+        "chance":self.current_chance,
+        "element_value":value,
+    },
+    opponent_id:{
+        "data":data,
+        "was_opponent":1,
+        "opponent_payoff":self.current_payoff,
+        "chance":self.current_chance,
+        "element_value":value,
+    }}
+
+
+Player.update_element = update_element
+    
---
+++ map_grid/pages.py
@@ -0,0 +1,56 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants,Player,Element
+import numpy as np
+class Instructions(Page):
+    def before_next_page(self):
+        self.player.current_payoff = 0
+class GridPlay(Page):
+    form_model = "player"
+    form_fields = ['current_x', 'current_y']
+    live_method = 'update_element'
+    def vars_for_template(self):
+        grid = []
+        for x in Constants.grid_x:
+            for y in Constants.grid_y:
+                grid.append(
+                    {
+                        'x': int(x),
+                        'y': int(y),
+                        'value': int(Constants.grid_value[x][y]),
+                        'occupied': not Element.objects.filter(x=x, y=y,player__in =self.player.group.get_players()).exists()
+                    }
+                ) 
+        other_players = self.player.group.get_players()
+        opponent_payoff = [player.current_payoff for player in other_players if player != self.player][0]
+        return {
+            'grid': grid,
+            'opponent_payoff': opponent_payoff,
+        }
+    def js_vars(self):
+        grid = []
+        for x in Constants.grid_x:
+            for y in Constants.grid_y:
+                grid.append(
+                    {
+                        'j': int(x),
+                        'i': int(y),
+                        'value': int(Constants.grid_value[x][y]),
+                    }
+                )
+
+        return {
+            'current_chance': self.player.current_chance,
+            'data': grid
+        }
+    def before_next_page(self):
+       pass
+class ResultsWaitPage(WaitPage):
+    pass
+
+
+class Results(Page):
+    pass
+
+
+page_sequence = [Instructions] + [GridPlay] +[ResultsWaitPage, Results]
